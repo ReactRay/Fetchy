eba9dacc00171e47da9210a26350417b03ab300e
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-05 13:55:42 +0300

    render first images

--- imagy-p/src/cmps/Gallary.jsx
+++ imagy-p/src/cmps/Gallary.jsx
@@ -1,10 +1,55 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query';
+
+
+
+const url = `https://api.unsplash.com/search/photos?query=shake&client_id=${import.meta.env.VITE_UNSPLASH_API_KEY}`;
 
 
 function Gallary() {
+    const response = useQuery({
+        queryKey: ['images'],
+        queryFn: async () => {
+            const res = await axios.get(url)
+
+            return res.data;
+        }
+    })
+
+    if (response.isLoading) {
+        return <section>
+            <h4>loading...</h4>
+        </section>
+    }
+
+    if (response.isError) {
+        return (
+            <section>
+                <h4>there was an error</h4>
+            </section>
+        )
+    }
+
+    const results = response.data.results;
+
+    if (results.length < 1) {
+        return (
+            <section>
+                <h4>no results found</h4>
+            </section>
+        )
+    }
+
     return (
-        <div>
-            <h2>Gallary</h2>
-        </div>
+        <section className='image-container'>
+            {results.map((item) => {
+                const url = item?.urls?.regular
+
+                return <img src={url} key={item.id} alt={item.alt_descreption} className='img' />
+            })}
+
+        </section>
     )
 }
 
--- imagy-p/src/main.jsx
+++ imagy-p/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import { AppProvider } from './stateManager/Context';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <AppProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+
     </AppProvider>
 
 
